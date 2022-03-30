eaf1ecfdad1f95e8a7429d6d6d43c520ea4716dc
kmetwally <kmetwally@silicon-mind.com>
2022-03-30 20:05:18 +0200

    automate whatsapp messages
    Signed-off-by: kmetwally <kmetwally@silicon-mind.com>

---
+++ script.py
@@ -0,0 +1,6 @@
+import pywhatkit as w
+import pyautogui
+
+w.sendwhatmsg_instantly("+201222210825", "مساء الخير، انا ق/ كربم حسام من كشافة نادي سبورتنج. لاحظنا تغيبكم الفترة اللي فاتت. ممكن تملوا الفورم ديه لمعرفة اسباب التغيب و نرجو حضوركم الاجتماع القادم يوم الجمعة الموافق 1/4/2022: https://forms.office.com/r/jGrsD4hZcT")
+pyautogui.click(1050, 950)
+pyautogui.press("enter")
